bef35851e5c31dad22386abc39148f1e4745b98b
Patrick <w.patrick.kelly@gmail.com>
2020-04-08 12:55:46 -0700

    starter html added

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
+  res.render('starter');
 });
 // Start Server
 
