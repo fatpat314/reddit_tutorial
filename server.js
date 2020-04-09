2c6054cf836f73e4eccfab1c9e19980b2740e5c3
Patrick <w.patrick.kelly@gmail.com>
2020-04-08 23:10:47 -0700

    MONGOD IS NOT WORKING FOR ME

---
+++ server.js
@@ -0,0 +1,42 @@
+// Require Libraries
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('starter');
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+});
+
+require('./controllers/posts.js')(app);
+
+require('./data/reddit-db');
+
+
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
