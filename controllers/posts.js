96b602cf9329138b2152cdeba6336f840e8cd2c8
Patrick <w.patrick.kelly@gmail.com>
2020-04-25 14:31:55 -0700

    dont work

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,34 +1,50 @@
 
 const Post = require('../models/posts.js');
-module.exports = (app) => {
+const User = require('../models/user.js');
+
+module.exports = function (app) {
     app.get('/', (req, res) => {
         var currentUser = req.user;
 
         Post.find({}).lean()
-        .then (posts => {
-            res.render("posts-index", { posts, currentUser });
-        })
-        .catch(err => {
-            console.log(err.message);
+            .then (posts => {
+                res.render("posts-index", { posts, currentUser });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
         });
-        // });
 
     app.get('/post/new', (req, res) => {
         // var currentUser = req.user;
         res.render('posts-new');
             });
   // CREATE
-  app.post("/posts/new", (req, res) => {
-      if (req.user) {
-          var post = new Post(req.body);
+  // CREATE
+      app.post("/posts/new", (req, res) => {
+          if (req.user) {
+              var post = new Post(req.body);
+              post.author = req.user._id;
 
-          post.save(function(err, post) {
-              return res.redirect(`/`);
-          });
-      } else {
-            return res.status(401); // UNAUTHORIZED
-        }
-});
+              post
+                  .save()
+                  .then(post => {
+                      return User.findById(req.user._id);
+                  })
+                  .then(user => {
+                      user.posts.unshift(post);
+                      user.save();
+                      // REDIRECT TO THE NEW POST
+                      res.redirect(`/posts/${post._id}`);
+                  })
+                  .catch(err => {
+                      console.log(err.message);
+                  });
+          } else {
+              return res.status(401); // UNAUTHORIZED
+          }
+      });
+
     app.get("/posts/:id", function(req, res){
         //lood up the post
         // LOOK UP THE POST
@@ -39,7 +55,7 @@ module.exports = (app) => {
         })
 
     });
-    });
+    // });
     // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
         Post.find({ subreddit: req.params.subreddit }).lean()
