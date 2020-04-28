24d6fc20ce928c302686c7b28fea13d9fc36bf70
Patrick <w.patrick.kelly@gmail.com>
2020-04-28 12:37:23 -0700

    log in tests

--- test/auth.js
+++ test/auth.js
@@ -35,4 +35,26 @@ after(function () {
   agent.close()
 });
 
+// login
+it("should be able to login", function(done) {
+  agent
+    .post("/login")
+    .send({ username: "testone", password: "password" })
+    .end(function(err, res) {
+      res.should.have.status(200);
+      agent.should.have.cookie("nToken");
+      done();
+    });
+});
+
+// logout
+it("should be able to logout", function(done) {
+  agent.get("/logout").end(function(err, res) {
+    res.should.have.status(200);
+    agent.should.not.have.cookie("nToken");
+    done();
+  });
+});
+
+
 });
