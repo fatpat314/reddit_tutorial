855825ac535737a811d2ade67e06d835db7211c2
Patrick <w.patrick.kelly@gmail.com>
2020-04-30 16:13:05 -0700

    implemented nested comments'

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ require('./data/reddit-db');
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+require('./controllers/replies.js')(app);
 
 
 module.exports = app;
