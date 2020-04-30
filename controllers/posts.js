3b454daaf094c8db7a94cede29b2194e8c7eac23
Patrick <w.patrick.kelly@gmail.com>
2020-04-30 15:35:25 -0700

    refactored populate code

--- controllers/posts.js
+++ controllers/posts.js
@@ -46,14 +46,11 @@ module.exports = function (app) {
       });
 
 
-
+      // SHOW
       app.get("/posts/:id", function (req, res) {
           var currentUser = req.user;
-          // LOOK UP THE POST
-
-          Post.findById(req.params.id).populate('comments').populate('author').lean()
+          Post.findById(req.params.id).populate('comments').lean()
               .then(post => {
-                  console.log(post)
                   res.render("posts-show", { post, currentUser });
               })
               .catch(err => {
@@ -63,16 +60,17 @@ module.exports = function (app) {
 
     // });
     // SUBREDDIT
+    // SUBREDDIT
     app.get("/n/:subreddit", function (req, res) {
         var currentUser = req.user;
-        Post.find({ subreddit: req.params.subreddit }).populate('author')
+        Post.find({ subreddit: req.params.subreddit }).lean()
             .then(posts => {
                 res.render("posts-index", { posts, currentUser });
             })
             .catch(err => {
                 console.log(err);
             });
-    });
+        });
 }
 //
 //     // app.get("/posts/:id", function(req, res){
--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Populate = require("../utils/autopopulate");
 
 const CommentSchema = new Schema({
   content: { type: String, required: true },
-  author : { type: Schema.Types.ObjectId, ref: "User"}
+  author : { type: Schema.Types.ObjectId, ref: "User"},
+  comments: [{type: Schema.Types.ObjectId, ref: "Comment"}]
 });
+
+CommentSchema
+    .pre('findOne', Populate('author'))
+    .pre('find', Populate('author'))
+    .pre('findOne', Populate('comments'))
+    .pre('find', Populate('comments'))
 
 module.exports = mongoose.model("Comment", CommentSchema);
